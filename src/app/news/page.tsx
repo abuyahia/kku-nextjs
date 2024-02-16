e1b12d5329bcc458989a10bf058735c46a49e93e
abuyahia <aabuyahia@kku.edu.sa>
2024-02-16 15:11:04 +0300

    All files project

---
+++ src/app/news/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from 'next/image';
+import LandingTemplate from '../../components/landing/LandingTemplate';
+
+export default async function newsList(){
+        
+    const response = await fetch("https://kku.edu.sa/ar/api/v1/news", {next:{ revalidate: 20 },});
+    const getNews = await response.json();
+    const newsList = getNews.map((news: any) => {
+        return (
+            <div className="components-news d-flex col-5 offset-1 flex-column" key={news.nid} >
+                    <h4>{news.title}</h4>     
+                    <Link href={`/news/${news.nid}`}>Link</Link>      
+                    <img className='col-6' src={`${news.image_large}`} alt="kku" />                                                                           
+            </div>		
+        )
+    });
+
+
+
+            return(
+                <LandingTemplate CTColor="red" CTName="News" CTPath="/news" image= "" title= "News"> 
+                    <div  className="d-flex flex-column flex-wrap container">
+                        <div  className="d-flex flex-row flex-wrap  col">                        
+                            {newsList}                         
+                        </div>
+                    </div>
+                </LandingTemplate>
+            );
+
+
+}
